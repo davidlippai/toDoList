01e633464e8760120fa0ac5f169c724a2db75170
davidlippai <lippai28@gmail.com>
2021-12-01 20:57:02 +0100

    ready 1.0.4

--- app.js
+++ app.js
@@ -4,39 +4,40 @@ let toDoInput = document.getElementById("toDoInput");
 let itemArrey = [];
 
 
-toDoButton.addEventListener('click', function(event){
-  event.preventDefault(); 
+toDoButton.addEventListener('click', function (event) {
+  event.preventDefault();
   //NEW ITEM 
-    let newItem = document.createElement("p");
+  let newItem = document.createElement("p");
 
-    newItem.innerText = toDoInput.value;
-    itemArrey.push(toDoInput.value);
-    if (toDoInput.value == ""){;
+  newItem.innerText = toDoInput.value;
+  itemArrey.push(toDoInput.value);
+  if (toDoInput.value == "") {
+    ;
     return alert('Please fill up the input field!');
   }
 
-    toDoContainer.appendChild(newItem);
-    toDoInput.value = "";
-    newItem.classList.add("newItemClass");
+  toDoContainer.appendChild(newItem);
+  toDoInput.value = "";
+  newItem.classList.add("newItemClass");
 
   // give an Option to delete the Item
-    let deleteOne = document.createElement("button");
-    newItem.appendChild(deleteOne);
-    deleteOne.innerHTML = '<i class="fa fa-trash"></i>';
-    deleteOne.classList.add("deleteOne");
-    deleteOne.addEventListener('click',function(){
-      toDoContainer.removeChild(newItem);
-    })
+  let deleteOne = document.createElement("button");
+  newItem.appendChild(deleteOne);
+  deleteOne.innerHTML = '<i class="fa fa-trash"></i>';
+  deleteOne.classList.add("deleteOne");
+  deleteOne.addEventListener('click', function () {
+    toDoContainer.removeChild(newItem);
+  })
 
   // give an Option to mark the Item as its DONE!
-    let itemDoneBtn = document.createElement("button");
-    newItem.appendChild(itemDoneBtn);
-    itemDoneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
-    itemDoneBtn.classList.add("itemDoneBtn");
+  let itemDoneBtn = document.createElement("button");
+  newItem.appendChild(itemDoneBtn);
+  itemDoneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  itemDoneBtn.classList.add("itemDoneBtn");
 
-    itemDoneBtn.addEventListener('click',function(){
-      newItem.classList.toggle("itemDone");
-    })
+  itemDoneBtn.addEventListener('click', function () {
+    newItem.classList.toggle("itemDone");
+  })
 
 
   /* Give a class to the Item what is already done!
@@ -45,9 +46,9 @@ toDoButton.addEventListener('click', function(event){
       itemDone = document.createElement("button");
     })*/
 
-  });
+});
 
-  if (itemArrey.length > 0) {
-    let deleteAllBtn = document.createElement("button");
-    deleteAllBtn.innerText = "delete all";
-  }
+if (itemArrey.length > 0) {
+  let deleteAllBtn = document.createElement("button");
+  deleteAllBtn.innerText = "delete all";
+}
